1661e7a73b63aea72d7c956fbf6a756b331667a8
ANSozontov <A.N.Sozontov@gmail.com>
2025-08-29 21:08:31 +0500

    code upd

--- code1.R
+++ code1.R
@@ -18,35 +18,41 @@ path <- dir() %>%
 
 izkn <- readxl::read_excel(path, sheet = "raw-data") %>% 
     select(RECORD:taxonRemarks) %>% 
-    select(-RECORD, -OCCURRENCE, -EVENT, -LOCATION, -TAXON)
+    select(-RECORD, -OCCURRENCE, -EVENT, -LOCATION, -TAXON) %>% 
+    mutate(
+        scientificName = str_replace_all(scientificName, "L. ", "L."),
+        scientificName = str_replace_all(scientificName, "C. ", "C."),
+        scientificName = str_replace_all(scientificName, "O. ", "O."))
+
+gen <- izkn %>% 
+    split(.$taxonRank) %>% 
+    map(~.x %>% 
+        pull(genus) %>% 
+        unique %>% 
+        sort
+    )
 
 lit.pln <- readxl::read_excel(path, sheet = "lib-data") %>% 
     select(RECORD:taxonRemarks) %>% 
     select(-RECORD, -OCCURRENCE, -EVENT, -LOCATION, -TAXON)
 
 lit.mnt <- filter(lit.pln, str_detect(dynamicProperties, "mountain"))
-lit.pln <- filter(lit.pln, str_detect(dynamicProperties, "plain") | is.na(dynamicProperties))
+lit.pln <- filter(lit.pln, str_detect(dynamicProperties, "plain"))
 
 gbif <- "0086127-250717081556266" %>% 
     occ_download_get(path = ".", overwrite = F) %>% 
     occ_download_import() %>% 
     filter(
         taxonRank %in% c("GENUS", "SPECIES", "SUBSPECIES"),
-        !is.na(decimalLatitude), 
-        !is.na(decimalLongitude)) %>%
+        !is.na(decimalLatitude) | !is.na(decimalLongitude)) %>%
     st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), 
              crs = 4326, remove = FALSE)
-gbif <- st_drop_geometry(gbif[st_intersects(gbif, kaz_bnd$focus, sparse = FALSE), ])
 
-inat <- gbif %>% 
-    filter(datasetName == "iNaturalist research-grade observations") %>% 
-    mutate(
-        eventDate = substr(eventDate, 1, 10), 
-        eventDate = as.Date(eventDate)) %>%
-    filter(eventDate <= as.Date("2025-04-18")) %>% 
-    filter(
-        # remove doubtful (orange) 
-        str_detect(scientificName, negate = TRUE, pattern = 
+gbif <- st_drop_geometry(gbif[st_intersects(gbif, kaz_bnd$focus, sparse = FALSE), ])
+inat <- gbif
+
+{
+sp.to.remove <- 
             "Aelurillus lutosus|Berlandina saraevi|Heriaeus horridus|
             |Heriaeus oblongus|Heterotheridion nigrovariegatum|
             |Hypsosinga kazachstanica|Larinia phthisica|Neoscona spasskyi|
@@ -54,24 +60,40 @@ inat <- gbif %>%
             |Poecilochroa variana|Pseudomogrus bucharaensis|
             |Runcinia tarabayevi|Tetragnatha nigrita|Thanatus fabricii|
             |Thanatus mongolicus|Eresus kollari|Euryopis flavomaculata|
-            |Araneus miquanensis")
+            |Araneus miquanensis"
+
+inat <- inat %>% 
+    filter(datasetName == "iNaturalist research-grade observations") %>% 
+    mutate(
+        eventDate = substr(eventDate, 1, 10), 
+        eventDate = as.Date(eventDate)) %>%
+    filter(eventDate <= as.Date("2025-04-18")) %>% 
+    filter(
+        # remove doubtful (orange) 
+        str_detect(scientificName, negate = TRUE, pattern = sp.to.remove)
     ) %>% 
     mutate(
         # replace Spiracme mongolica -> Xysticus mongolicus 
         scientificName = case_when(
             str_detect(scientificName, "Spiracme mongolica") ~ "Xysticus mongolicus", 
             TRUE ~ scientificName),
-        scientificName = case_when(
-            str_detect(scientificName, "Xysticus cristatus") ~ "Xysticus pseudocristatus", 
-            TRUE ~ scientificName),
+        species = case_when(
+            str_detect(species, "Spiracme mongolica") ~ "Xysticus mongolicus", 
+            TRUE ~ species),
         # replace Xysticus cristatus -> X. pseudocristatus
         scientificName = case_when(
             str_detect(scientificName, "Xysticus cristatus") ~ "Xysticus pseudocristatus", 
             TRUE ~ scientificName),
+        species = case_when(
+            str_detect(species, "Xysticus cristatus") ~ "Xysticus pseudocristatus", 
+            TRUE ~ species),
         # replace F. mongolica -> Fedotovia uzbekistanica
         scientificName = case_when(
             str_detect(scientificName, "Fedotovia mongolica") ~ "Fedotovia uzbekistanica", 
             TRUE ~ scientificName),
+        species = case_when(
+            str_detect(species, "Fedotovia mongolica") ~ "Fedotovia uzbekistanica", 
+            TRUE ~ species),
         # genreralize to genera (yellow)
         taxonRank = case_when(
             str_detect(scientificName, "Alopecosa fedotovi|Alopecosa hui") ~ "GENUS", 
@@ -80,15 +102,19 @@ inat <- gbif %>%
         scientificName = case_when(
             str_detect(scientificName, "Alopecosa fedotovi|Alopecosa hui") ~ "Alopecosa", 
             TRUE ~ scientificName
+        ),
+        species = case_when(
+            str_detect(species, "Alopecosa fedotovi|Alopecosa hui") ~ "Alopecosa", 
+            TRUE ~ species
         )
         
     )
-
+}
 gbif <- gbif %>% 
     filter(datasetName != "iNaturalist research-grade observations")
-cat("end data load")   
-# geo ---------------------------------------------------------------------
-geo <- lst(izkn, lit.pln, lit.mnt, gbif, inat) %>%
+cat("data are here")
+# MAP ---------------------------------------------------------------------
+geo_plot <- lst(izkn, lit.pln, lit.mnt, gbif, inat) %>%
     map(~.x %>% 
         separate(scientificName, c("g", "sp"), sep = " ", extra = "drop") %>% 
         mutate(taxa = case_when(
@@ -97,13 +123,6 @@ geo <- lst(izkn, lit.pln, lit.mnt, gbif, inat) %>%
         )) %>% 
         select(occurrenceID, taxa, taxonRank, decimalLatitude, decimalLongitude) %>% 
         filter(!is.na(decimalLatitude), !is.na(decimalLongitude)) %>%
-        st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), 
-                 crs = 4326, remove = FALSE)
-    )
-
-geo_plot <- geo %>% 
-    map(~.x %>% 
-        st_drop_geometry %>% 
         mutate(
             decimalLongitude = round(as.numeric(decimalLongitude), 3), 
             decimalLatitude = round(as.numeric(decimalLatitude), 3)) %>% 
@@ -173,18 +192,21 @@ leaflet() %>%
             "literature (mountain)"),
         options = layersControlOptions(collapsed = FALSE)
     ) %>%
-    addScaleBar()
+    addScaleBar(position = "bottomright")
 
 # counts ------------------------------------------------------------------
-sp_list <- lst(
+sp_list_list <- lst(
     izkn = mutate(izkn, taxonRemarks = paste0(identificationRemarks, ", ", taxonRemarks)),
-    lit.pln, gbif, inat) %>% sapply(nrow)
+    lit.pln, gbif, inat) %>% 
     map(~.x %>% 
-        st_drop_geometry %>% 
-        filter(taxonRank != "FAMILY") %>% 
-        transmute(family, taxonRank, taxonRemarks, 
-               scientificName = case_when(taxonRank == "GENUS"~ genus, TRUE ~species)
-        )) %>% 
+            st_drop_geometry %>% 
+            filter(taxonRank != "FAMILY") %>% 
+            transmute(family, taxonRank, 
+                      taxonRemarks = as.character(taxonRemarks),
+                      scientificName #= case_when(taxonRank == "GENUS"~ genus, TRUE ~species)
+            ))
+
+sp_list_df <- sp_list_list %>% 
     map_dfr(rbind, .id = "dataset") %>% 
     filter(taxonRank != "GENUS" | str_detect(scientificName, "Pritha|Segestria|Pirata|Hylyphantes")) %>% 
     distinct() %>% 
@@ -195,31 +217,134 @@ sp_list <- lst(
         taxonRemarks = str_replace_all(taxonRemarks, ", NA", ""),
         taxonRemarks = case_when(taxonRemarks == "NA" ~ NA, TRUE ~ taxonRemarks))
 
-remarks <- sp_list %>% 
+remarks <- sp_list_df %>% 
     group_by(scientificName) %>% 
     summarise(taxonRemarks = paste0(na.omit(taxonRemarks), collapse = ", "), .groups = "drop")
-    
-sp_list <- sp_list %>% 
+
+sp_list_df <- sp_list_df %>% 
     select(-taxonRemarks) %>% 
     distinct() %>% 
-    pivot_wider(names_from = dataset, values_from = status, values_fill = 0, values_fn = sum) %>% 
+    pivot_wider(names_from = dataset, values_from = status, values_fill = NA, values_fn = sum) %>% 
     left_join(remarks, by = "scientificName")
 
-writexl::write_xlsx(sp_list, "species_list.xlsx")
+sum(izkn$individualCount)
+
+# IZKN collectors
+IZKN_collectors <- izkn %>% 
+    pull(recordedBy) %>% 
+    str_split(", ") %>% 
+    flatten_chr() %>% 
+    table %>% 
+    tibble(name = names(.), n = as.numeric(.)) %>% 
+    select(name, n) %>% 
+    arrange(desc(n))
+
+# izkn %>% 
+#     filter(taxonRank == "SPECIES" | genus %in% gen$GENUS[!(gen$GENUS %in% gen$SPECIES)]) %>% 
+#     select(scientificName) %>% 
+#     distinct() %>% 
+#     arrange(scientificName) 
+
+# Figures -----------------------------------------------------------------
+# Vienn Diagram
+library(VennDiagram)
+sp_list_list %>% 
+    map(~.x %>% 
+        distinct %>% 
+        pull(scientificName) %>% 
+        unique %>% 
+        sort
+    ) %>% 
+    venn.diagram(
+        x = .,
+        category.names = c("IZKN" , "Liter." , 
+                           "GBIF", "iNat"),
+        filename = paste0('venn.diagramm_', Sys.Date(), '.png'),
+        disable.logging = TRUE,
+        output=TRUE,
+        
+        # Output features
+        imagetype="png" ,
+        height = 880 , 
+        width = 880 , 
+        resolution = 300,
+        compression = "lzw",
+        
+        # Circles
+        lwd = 2,
+        lty = 'blank',
+        fill = c("red", "blue", "green", "yellow"),
+        
+        # Numbers
+        cex = .6,
+        fontface = "bold",
+        fontfamily = "sans",
+        
+        # Set names
+        cat.cex = 0.6, 
+        cat.fontface = "bold", 
+        cat.default.pos = "outer",
+        # cat.pos = c(-27, 27, 135),
+        # cat.dist = c(0.055, 0.055, 0.085),
+        cat.fontfamily = "sans"
+        # rotation = 1
+    ) 
+
+sp_list_list %>% 
+    map(distinct) %>% 
+    map_df(rbind, .id = "id") %>% 
+    select(id, family, scientificName) %>% 
+    rbind(mutate(., id = "total")) %>% 
+    distinct() %>% 
+    count(id, family) %>%  
+    mutate(family  = case_when(family %in% c(
+        "Salticidae", "Gnaphosidae", "Lycosidae", "Thomisidae", 
+        "Araneidae", "Philodromidae", "Theridiidae", "Linyphiidae"
+        ) ~ family, TRUE ~ "x.others"
+    )) %>% 
+    group_by(id, family) %>% 
+    summarise(nn = sum(n), .groups = "drop_last") %>% 
+    mutate(
+        nnn = nn / sum(nn) * 100, 
+        nnnn = round(nnn, 1)
+        ) %>% 
+    ungroup %>% 
+    mutate(
+        id = toupper(id),
+        family = factor(family, ordered = TRUE)) %>% 
+    ggplot(aes(x = "", y = nnn, fill = family, label = nnnn)) +
+    geom_bar(stat = "identity", width = 1, color = "black") +
+    # geom_label(
+    #            alpha = 0.7,
+    #            nudge_x = 0.6,
+    #            size = 2.2) +
+    scale_fill_brewer(palette = "Set3") +
+    labs(fill = "Family", x = NULL, y = NULL) +
+    # guides(fill = guide_legend(override.aes = list(label = NULL, colour = NA), reverse = TRUE)) +
+    coord_polar("y") +
+    scale_y_reverse() +
+    facet_wrap(~ id) + 
+    theme_void() 
+            
 
 # export ------------------------------------------------------------------
+lst(IZKN_collectors)
+writexl::write_xlsx()
+
+writexl::write_xlsx(sp_list_df, "species_list.xlsx")
+
 contains_cyrillic <- function(text) {
     text <- paste0(text, collapse = "")
     str_detect(text, "[\\u0400-\\u04FF]")
 }
 
-rbind(lit.mnt, lit.pln) %>% 
+lit <- rbind(lit.mnt, lit.pln)
+lit %>% 
     #убирать перед проверкой на кириллицу
     select(-starts_with("verba"), -associatedReferences, -bibliographicCitation) %>% 
     sapply(contains_cyrillic)
      
-
-
+readr::write_delim(lit, "occ_literature.csv", delim = "\t")
 
 # arj ---------------------------------------------------------------------
 x <- unzip("0086127-250717081556266.zip", "occurrence.txt") %>% 
